08913fc9fe047abf9cbf610b5f99b489905e2bcb
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2024-11-26 22:54:50 +0900

    feedback 구현 완료

--- src/main/java/com/mallang/backend/dto/FeedbackDTO.java
+++ src/main/java/com/mallang/backend/dto/FeedbackDTO.java
@@ -9,5 +9,4 @@ public class FeedbackDTO {
     private String name;
     private String briefContent; // 내용 요약 (한 줄)
     private LocalDateTime createdDate;
-    private String status; // "안 읽음" 또는 "읽음"
 }
