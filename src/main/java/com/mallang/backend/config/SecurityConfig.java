ceae3fbb1b2db78bd85291feb3b942654105ab88
CorrectHwan <158758591+CorrectHwan@users.noreply.github.com>
2024-11-26 22:50:05 +0900

    feedback 구현 완료

--- src/main/java/com/mallang/backend/config/SecurityConfig.java
+++ src/main/java/com/mallang/backend/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/member/join", "/", "/error", "/login").permitAll() // 인증 없이 접근 가능
                 .requestMatchers("/api/admin").hasRole("ADMIN") // 관리자만 접근 가능
                 .anyRequest().authenticated() // 다른 요청은 로그인한 사용자만 접근 가능
+                .requestMatchers("/api/feedback/**").permitAll() // feedback 엔드포인트 인증 없이 접근 가능
         );
 
         //JWTFilter 등록
